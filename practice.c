f1b32705cba6494aee28a8bab79cc5c0879b2822
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-08 22:31:00 +0500

    program 1 (c program)
    This program displays the sum of two numbers.

---
+++ practice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,sum;
+	
+	printf("Enter 1 number=");
+	scanf("%d",&a);
+	printf("Enter 2 number=");
+	scanf("%d",&b);
+	sum = a + b;
+	printf("sum is =%d ",sum);
+	getch();
+
+	
+}
