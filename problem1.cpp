a43a23d126b2b13b223a3f20583ccd9efdf5e862
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2022-08-08 22:23:01 +0500

    program 1
    This program will take input from the user until the user presses the number "42".
    For now, it is limited to 10 elements of an array.

---
+++ problem1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int arr[10],i;
+		cout<<"Enter a number =";
+for(int a=1;a<=10;a++)
+	{	
+		cin>> arr[i];
+		
+		if (arr[i] != 42)
+	
+	{
+		cout<<"Enter a number =";
+	
+	}
+	else{
+		cout<<"Program terminated bcz you entered the number="<<arr[i];
+		break;
+	}
+	
+	}
+	
+}
